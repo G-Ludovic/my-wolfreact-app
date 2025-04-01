1b7e0d2068942d257d4c1c37d2479085c9a006a1
Ludovic G. <l.galicher@free.fr>
2025-04-01 15:33:32 +0200

    create component PokemonCard for Bulbasaur

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,10 @@
+function PokemonCard () {
+    return <>
+    <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image représentant Bulbizarre"></img>
+    </figure>
+    <figcaption>Bulbizarre</figcaption>
+    </>
+}
+
+export default PokemonCard;
