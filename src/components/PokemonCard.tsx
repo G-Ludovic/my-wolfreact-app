93fa6eceaaddd9ffec0778ea2a74b12a7b393ad6
Ludovic G. <l.galicher@free.fr>
2025-04-01 18:43:52 +0200

    dynamic PokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,25 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard () {
-    return <>
-    <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image représentant Bulbizarre"></img>
-    </figure>
-    <figcaption>Bulbizarre</figcaption>
-    </>
-}
+    const pokemon = pokemonList[1];
+        return (  
+        <>
+        {pokemon.imgSrc ? (
+            <figure>
+                <img src={pokemon.imgSrc} alt={`image représentant ${pokemon.name}`} />
+                <figcaption>{pokemon.name}</figcaption>
+            </figure> ) : (<p>???</p>)}
+        </> 
+        );
+};
 
 export default PokemonCard;
