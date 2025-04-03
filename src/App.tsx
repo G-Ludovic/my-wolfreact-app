61f7c306b8efdce0c6d51ab82ceed86d250f57fd
Ludovic G. <l.galicher@free.fr>
2025-04-03 16:10:49 +0200

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 import { useState } from "react";
 
-
 const pokemonList = [
   {
     name: "bulbasaur",
@@ -10,24 +9,43 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
-
-  const [pokemonIndex, setPokemonName] = useState(0);
-  const pokemon = pokemonList[pokemonIndex];
-
+  const [pokemonIndex, setPokemonIndex] = useState("bulbasaur"); 
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonIndex);           
+ 
   if (pokemon == null) {
-    throw new Error("Invalid pokemon name");
+    return <p>Pokémon introuvable</p> // Si pokemon est undefined, on affiche "Pokémon introuvable" pour éviter une erreur
   };
 
   return (
     <>
-    <PokemonCard pokemon={pokemon} />
-    <button onClick={() => setPokemonName(0)}>bulbasaur</button>
-    <button onClick={() => setPokemonName(1)}>mew</button>
+      <nav>
+        {pokemonList.map((po) => (  // pokemonList.map(...) génère une liste de boutons, un par Pokémon et "po" représente la clé de chaque pokémon
+          <button key={po.name} onClick={() => setPokemonIndex(po.name)}>  
+            {po.name}
+          </button>
+        ))} 
+      </nav>
+      <PokemonCard pokemon={pokemon} />
     </>
   );
 };
